eebca78931678a8a18f6ac5c1d310e6e92ebe9d7
Peter Georgeson <peter@supernifty.org>
2020-09-24 12:15:44 +1000

    join points on scatter

--- plotme/scatter.py
+++ plotme/scatter.py
@@ -17,7 +17,7 @@ import plotme.settings
 COLORS = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 MARKERS = ('o', 'x', 'v', '^', '<', '>', '1', '2', '3', '4', '8', 's', 'p', 'P', '*', 'h', 'H', '+', 'X', 'D', 'd', '.', ',', '|', '_')
 
-def plot_scatter(data_fh, target, xlabel, ylabel, zlabel, figsize, fontsize, log, title, x_label, y_label, wiggle, delimiter, z_color):
+def plot_scatter(data_fh, target, xlabel, ylabel, zlabel, figsize, fontsize, log, title, x_label, y_label, wiggle, delimiter, z_color, join):
   logging.info('starting...')
   matplotlib.style.use('seaborn')
 
@@ -83,8 +83,12 @@ def plot_scatter(data_fh, target, xlabel, ylabel, zlabel, figsize, fontsize, log
       vals = [list(x) for x in zip(xvals, yvals, zvals, cvals, mvals) if x[2] == zval]
       ax.scatter([x[0] for x in vals], [x[1] for x in vals], c=[x[3] for x in vals], marker=vals[0][4], label=zval, alpha=0.8)
       ax.legend()
+      if join: # TODO does this work?
+        ax.join([x[0] for x in vals], [x[1] for x in vals], c=[x[3] for x in vals], marker=vals[0][4], label=zval, alpha=0.8)
   else:
     ax.scatter(xvals, yvals)
+    if join:
+      ax.plot(xvals, yvals)
 
   if zlabel is not None:
     if not z_color:
@@ -113,6 +117,7 @@ if __name__ == '__main__':
   parser.add_argument('--wiggle', required=False, default=0, type=float, help='randomly perturb data')
   parser.add_argument('--delimiter', required=False, default='\t', help='input file delimiter')
   parser.add_argument('--log', action='store_true', help='log z')
+  parser.add_argument('--join', action='store_true', help='join points')
   parser.add_argument('--verbose', action='store_true', help='more logging')
   parser.add_argument('--target', required=False, default='plot.png', help='plot filename')
   args = parser.parse_args()
@@ -121,4 +126,4 @@ if __name__ == '__main__':
   else:
     logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
 
-  plot_scatter(sys.stdin, args.target, args.x, args.y, args.z, args.figsize, args.fontsize, args.log, args.title, args.x_label, args.y_label, args.wiggle, args.delimiter, args.z_color)
+  plot_scatter(sys.stdin, args.target, args.x, args.y, args.z, args.figsize, args.fontsize, args.log, args.title, args.x_label, args.y_label, args.wiggle, args.delimiter, args.z_color, args.join)
